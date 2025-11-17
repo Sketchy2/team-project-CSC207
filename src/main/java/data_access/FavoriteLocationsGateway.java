ecfb289366f9e727b9b1bd76303b833f93fa5a23
elizaxxx7 <Elizabeth_han@hotmail.com>
2025-11-17 15:33:26 -0500

    delete unused and refine favlocationGateway

--- src/main/java/data_access/FavoriteLocationsGateway.java
+++ src/main/java/data_access/FavoriteLocationsGateway.java
@@ -5,17 +5,18 @@ import java.util.List;
 public interface FavoriteLocationsGateway {
     /**Returns true if this city is already saved as a favorite.*/
     boolean existsByName(String cityName);
-    void save(String cityName);
-    void delete(String cityName);
 
     /**
-     * Tries to save a favorite.
+     * Tries to add a favorite.
      * @return true if saved successfully, false if storage error.
      */
     boolean saveFavorites(String cityName);
+
+    /** Delete a favorite city by name. */
+    void delete(String cityName);
+
     /** Returns all saved favorite city names. */
     List<String> getFavorites();
-
 
 }
 
