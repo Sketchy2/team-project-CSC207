5c1a8c37fe063a8654dc3ee689ab942bab46ec54
CarlGergi <carl.gergi@mail.utoronto.ca>
2025-11-19 15:54:51 -0500

    implemented delete saved outfit feature

--- src/main/java/use_case/delete/DeleteOutfitOutputData.java
+++ src/main/java/use_case/delete/DeleteOutfitOutputData.java
@@ -1,1 +1,17 @@
 package use_case.delete;
+import entities.Outfit;
+import java.util.List;
+
+public class DeleteOutfitOutputData {
+    private final List<Outfit> outfits;
+    private final String message;
+
+    public DeleteOutfitOutputData(List<Outfit> outfits, String message) {
+        this.outfits = outfits;
+        this.message = message;
+    }
+
+    public List<Outfit> getOutfits() { return outfits; }
+    public String getMessage() { return message; }
+}
+
