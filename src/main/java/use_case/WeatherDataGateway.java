43fd4e58fe8e43548448008585f014120a5c8014
CarlGergi <carl.gergi@mail.utoronto.ca>
2025-11-30 19:47:24 -0500

    Fixed coutry code bug + fixed uc 4 program now works very well

--- src/main/java/use_case/WeatherDataGateway.java
+++ src/main/java/use_case/WeatherDataGateway.java
@@ -5,6 +5,9 @@ import entities.WeatherData;
 import entities.WeeklyData;
 
 public interface WeatherDataGateway {
+    // Added to allow resolving location (and country code) before fetching weather
+    Location searchLocation(String cityName);
+    
     WeatherData fetch(Location location);
     WeeklyData fetchWeekly(Location location);
 }
